c2c6480615097e5bb457189648d82aa206d2a5b9
TJ Kandala <kandalatj@gmail.com>
2021-08-03 16:48:34 -0400

    add latency to extension requests

--- import-scripts-only/serve.js
+++ import-scripts-only/serve.js
@@ -6,6 +6,16 @@ const app = express();
 // TODO
 
 // Serve webpage
+app.use((req, res, next) => {
+  // fake latency to emulate real extension registry
+  if (req.path.endsWith("dist/index.js")) {
+    setTimeout(() => {
+      next();
+    }, 1000);
+  } else {
+    next();
+  }
+});
 app.use(express.static(path.join(__dirname, "src")));
 app.listen(3000, () => {
   console.log("Web Worker demo listening on port 3000");
