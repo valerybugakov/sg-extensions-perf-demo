e18b626c44c22bab0a57ac5baf8617f277f3936f
TJ Kandala <kandalatj@gmail.com>
2021-08-05 15:36:16 -0400

    make importScripts loader error tolerant

--- dyn-import-with-fallback/src/extensions/3/index.js
+++ dyn-import-with-fallback/src/extensions/3/index.js
@@ -5,3 +5,7 @@ console.log("importing extension three");
 export function activate() {
     sourcegraph.app.log("activating extension three");
 }
+
+export function deactivate() {
+    sourcegraph.app.log("deactivating extension three");
+}
--- dyn-import-with-fallback/src/worker.js
+++ dyn-import-with-fallback/src/worker.js
@@ -31,19 +31,15 @@ function createAPI() {
  * Known contexts in which this returns `false`:
  * - Web Worker on Firefox
  *  */
-const supportsDynamicImport = (async () => {
+const supportsDynamicImportPromise = (async () => {
   try {
     await import("data:text/javascript;charset=utf-8,");
     return true;
   } catch (error) {
-    console.error(error);
+    // console.error(error);
     return false;
   }
 })();
-
-supportsDynamicImport.then((isImplemented) =>
-  console.log("is import() implemented:", isImplemented)
-);
 
 // queue of API contexts
 const initContextQueue = [];
@@ -59,7 +55,39 @@ globalThis.require = () => {
 
 const exports = {};
 self.exports = exports;
+// self.module = new Proxy(
+//   { exports },
+//   {
+//     get: (target, prop, receiver) => {
+//       console.log({ target, prop, receiver });
+//       return {
+//         activate: () => console.log("proxy activate"),
+//       };
+//     },
+//     set: (obj, prop, value) => {
+//       console.log({ obj, prop, value });
+//       return true;
+//     },
+//   }
+// );
 self.module = { exports };
+
+loadExtensions(extensionIDs);
+
+async function loadExtensions(ids) {
+  const supportsDynamicImport = await supportsDynamicImportPromise;
+
+  console.log({ supportsDynamicImport });
+
+  if (supportsDynamicImport) {
+    await importLoader(ids);
+  } else {
+    importScriptsLoader(ids);
+  }
+}
+
+// LOADER TYPE:
+// - returns Promise for map of extension id -> activate + deactivate functions.
 
 // Even when all extensions are returned at the same time, seems to be:
 //   extension execution: 1-2-3----
@@ -71,19 +99,58 @@ self.module = { exports };
 // as opposed to
 //   extension execution: 123---
 // promise callback call: ---123
-// Promise.all(
-//   extensionIDs.map((id) =>
-//     import(`./extensions/${id}/dist/index.js`)
-//       .then((module) => {
-//         // queueMicrotask(() => console.log(`microtask: ${id}`));
-//         setTimeout(() => console.log(`timeout: ${id}`), 0);
+async function importLoader(ids) {
+  await Promise.all(
+    ids.map((id) =>
+      import(`./extensions/${id}/dist/index.js`)
+        .then((module) => {
+          // queueMicrotask(() => console.log(`microtask: ${id}`));
+          setTimeout(() => console.log(`timeout: ${id}`), 0);
 
-//         const extensionExports = self.module.exports;
+          const extensionExports = self.module.exports;
 
-//         initContextQueue.shift()(id);
+          initContextQueue.shift()(id);
 
-//         extensionExports.activate();
-//       })
-//       .catch((error) => console.log(error))
-//   )
-// );
+          extensionExports.activate();
+        })
+        .catch((error) => console.log(`error importing ${id}`, error))
+    )
+  );
+}
+
+function importScriptsLoader(ids) {
+  let nextImportIndex = 0;
+
+  const exports = {};
+  self.exports = exports;
+  self.module = new Proxy(
+    { exports },
+    {
+      get: (target, prop, receiver) => {
+        console.log({ target, prop, receiver });
+        return {
+          activate: () => console.log("proxy activate"),
+        };
+      },
+      set: (obj, prop, value) => {
+        console.log({ obj, prop, value });
+        if (typeof value === "object" && "activate" in value) {
+          // We assume that it is a successful import if the extension was able to export its activate function.
+          nextImportIndex++;
+        }
+        return true;
+      },
+    }
+  );
+
+  while (nextImportIndex < ids.length) {
+    try {
+      const restIds = ids.slice(nextImportIndex);
+      importScripts(...restIds.map((id) => `./extensions/${id}/dist/index.js`));
+    } catch (error) {
+      console.log("error importing scripts", { nextImportIndex, error });
+      nextImportIndex++;
+    }
+  }
+  console.log({ initContextQueue });
+}
